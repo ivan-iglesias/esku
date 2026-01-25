9e2be3882237ae3fde04bdc8304bd1979d9fd2c8
Ivan Iglesias <i.iglesias.bilbao@gmail.com>
2026-01-25 18:17:40 +0100

    Add vue ts checker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      typescript: true,
+      vueTsc: true,
+    }),
+  ],
   server: {
     port: 5173,
     proxy: {
